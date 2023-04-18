55996958ba354b7e90fb407024d01c6cde448f66
CouffignalCedric <cedric.couffignal.sncf@gmail.com>
2023-04-18 18:34:14 +0200

    ajout background sur pages annonces

--- src/app/proposition-troc/proposition-troc.component.ts
+++ src/app/proposition-troc/proposition-troc.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NotificationService } from '../notification.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-proposition-troc',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropositionTrocComponent implements OnInit {
 
-  constructor() { }
+  content!: String;
+  sender!: String;
+
+  constructor(private notificationService: NotificationService,private router: Router) { }
 
   ngOnInit() {
   }
+  sendProposition() {
+   this.notificationService.createNotification(this.content,this.sender).subscribe(prop =>{
+    console.log(prop);
+    this.router.navigate(['barters']);// retour a la page annonces après ajout d'une proposition
+   });
+    }
+}// addAnnonce() {
+//   if (!this.checkDescriptionLength()) {
+//     return;
+//   }
+//   this.annonceService.addOneAnnonce(this.newAnnonce).subscribe(ann => {
+//     console.log(ann);
+//     this.router.navigate(['barters']);// retour a la page annonces après ajout d'une annonce
+//   });
+// }
+// newAnnonce = new Annonce();
+// id!: number;
+// annoucement_picture!: string;
+// description!: string;
+// message!: string;              // ajout d'attribut 
 
-}
+
+
+// constructor(private annonceService: AnnonceService, private router: Router) { }
+
+// ngOnInit() {
+
+// }
+
+
