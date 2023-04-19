9887f94c5f63c790f1e08acf30e0a1b498b5a0fe
Laurie77170 <[Laurie77170](mailto:lauriebou1984@gmail.com)>
2023-04-19 17:26:08 +0200

    up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,26 +7,8 @@ import { NavigationEnd, Router } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'P3_Front';
 
-  // Propriété qui va déterminer si le footer doit être masqué ou affiché
-  hiddenFooter: boolean = true;
 
-  constructor(private router: Router) {}
-
-  ngOnInit() {
-  // écoute les évènements de navigation du 'Router'
-    this.router.events.subscribe((event) => {
-      // si l'évènement est de type 'NavigationEnd', la navigation sera terminée
-      if (event instanceof NavigationEnd) {
-        this.hiddenFooter = !this.hidden();
-      }
-      
-    });    
-  }
-
-  hidden() {
-    return this.router.url === '/' || this.router.url === '/accueil';
-  }
 }
--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,16 +1,38 @@
-import { Component } from '@angular/core';
-// import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent {
-affiche: any;
+export class FooterComponent implements OnInit {
+
+
+  // Propriété qui va déterminer si le footer doit être masqué ou affiché
+  hiddenFooter: boolean = true;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+  // écoute les évènements de navigation du 'Router'
+    this.router.events.subscribe((event) => {
+      // si l'évènement est de type 'NavigationEnd', la navigation sera terminée
+      if (event instanceof NavigationEnd) {
+        this.hiddenFooter = !this.hidden();
+      }
+      
+    });    
+  }
+
+  hidden() {
+    return this.router.url === '/' || this.router.url === '/accueil';
+  }
 
   // hidden: boolean = true;
 
+ 
   
     
     // if (this.router.url === '/' || this.router.url === '/accueil') {
@@ -19,7 +41,7 @@ affiche: any;
 
   
 
-  }
+}
 
   
 
