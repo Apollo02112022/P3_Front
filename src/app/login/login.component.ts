abd044461db252f25063024cdbd330bd8ebdfaa1
samfisher <samy.perissutti@gmail.com>
2023-04-19 16:01:10 +0200

    up form with Nics la frite

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,5 +8,41 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+
+    registerForm = new FormGroup({
+      pseudo: new FormControl("", [Validators.required, Validators.maxLength(10)]),
+      password: new FormControl("", [Validators.required, Validators.minLength(8)])
+    });
+
+   login() {
+    const { pseudo, password } = this.registerForm.value;
+    const data = { pseudo, password };
+    fetch("http://localhost:8080/login", {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then(res => res.json())
+      .then(response => {
+        const { token } = response;
+        localStorage.setItem("token", token);
+        console.log(token, "Connecté avec Success !")
+      })
+      .catch(err => console.log(err))
+  }
+
+   testWithToken = () => {
+    const token = localStorage.getItem("token");
+    const header = new Headers({ 'Authorization': `Bearer ${token}`, "Content-Type": "application/json" });
+    const options = {
+      headers: header,
+    };
+    fetch("http://localhost:8080/try", options)
+      .then(res => res.json())
+      .then(response => console.log(response))
+      .catch(err => console.log(err))
+  }
 
 }
