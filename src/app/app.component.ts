4f0c2e090c330ad0e72e5014981d76509022f53b
samfisher <samy.perissutti@gmail.com>
2023-05-04 10:17:03 +0200

    up token login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
@@ -7,5 +10,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'P3_Front';
+
+  constructor(private http: HttpClient) {}    
+
+  getData(): Observable<any> {
+    const url = 'http://localhost:8080/offer-a-barter';
+    const token = localStorage.getItem("token");
+    const headers = new HttpHeaders({ 'Authorization': `Bearer ${token}`, "Content-Type": "application/json" });
+    const options = {
+      headers: headers,
+    };
+  
+    return this.http.get<any>(url, options);
+  }
 
 }
